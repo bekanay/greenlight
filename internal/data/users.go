3d74077d652a58d6c335f9bd76f8aadc65ee8c52
bekanay <beki.bekovich2004@gmail.com>
2025-10-21 09:52:04 +0500

    370 page Chapter 17

--- internal/data/users.go
+++ internal/data/users.go
@@ -16,14 +16,20 @@ var (
 	ErrDuplicateEmail = errors.New("duplicate email")
 )
 
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
-	Password  password  `json:"activated"`
+	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 type password struct {
